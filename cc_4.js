1ad135ae97f4c067946aa88b08b2af718fdcb346
Isabella Miranda <ifmiranda@usf.edu>
2025-09-19 14:22:41 -0400

    Products established

---
+++ cc_4.js
@@ -0,0 +1,11 @@
+// Coding Challenge #04 
+
+// Step 2: Product Array 
+const products = [ 
+    { name: "Airpods Max", category: "electronics", price: 500, inventory: 25 }, 
+    { name: "Aritzia sweatset", category: "apparel", price:50, inventory:30 }, 
+    { name: "Organic lemons", category: "groceries", price: 5, inventory: 50 }, 
+    { name: "Clorox wipes", category: "household", price:6, inventory: 60 }, 
+    { name: "Hourglass blush", category: "beauty", price: 39, inventory: 20}
+]; 
+// Step 3: 
